dc75b52ecc31a3f938ac0f76c0471e32c875010e
Pavlova Marina <marizza.mil89@gmail.com>
2021-05-02 21:36:22 +0300

    Add project files.

---
+++ WPF_EXAM_3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_EXAM_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.vm = new MainWindowViewModel();
+            this.DataContext = this.vm;
+        }
+
+        public void SafeInvoke(Action action)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                action.Invoke();
+                return;
+            }
+            else
+            {
+                this.Dispatcher.BeginInvoke(action);
+            }
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Task.Run(() =>
+            {
+                    Action uiwork = () => sizeText.Text = vm.ReadUrl(vm.URL);
+                    SafeInvoke(uiwork);
+                    Thread.Sleep(1000);
+            });
+           lbl.Content = $"The total time elapsed is: {stopwatch.Elapsed.TotalMilliseconds} ms";
+            stopwatch.Stop();
+        }
+    }
+    
+}
---
+++ WPF_EXAM_3/MainWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WPF_EXAM_3
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        
+        public string URL { get; set; }
+        public DelegateCommand MyDelegate { get; set; }
+
+        private string m_size;
+        private bool m_flag;
+
+        public string SizeValue
+        {
+            get
+            {
+                return this.m_size;
+            }
+            set
+            {
+                this.m_size = value;
+                OnPropertyChanged("SizeValue");
+            }
+        }
+
+        public bool FlagIsEnabled
+        {
+            get
+            {
+                return this.m_flag;
+            }
+            set
+            {
+                this.m_flag = value;
+                OnPropertyChanged("FlagIsEnabled");
+            }
+        }
+        public MainWindowViewModel()
+        {
+            MyDelegate = new DelegateCommand(ExecuteCommand, CanExecuteMethod);
+            SizeValue = "Please Wait...";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        private bool CanExecuteMethod() { return true; }
+
+        private void ExecuteCommand()
+        {
+            FlagIsEnabled = false;
+        }
+        public string ReadUrl(string url)
+        {
+            try
+            {
+                WebRequest webRequest = WebRequest.Create(url);
+                WebResponse response = webRequest.GetResponseAsync().Result;
+
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string text = reader.ReadToEndAsync().Result;
+                    SizeValue = text.Length.ToString();
+                }
+                FlagIsEnabled = true;
+            }
+            catch (IOException ioex)
+            {
+                throw new IOException("An error occurred while processing the link.", ioex);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("An generic error ocurred.");
+            }
+            return SizeValue;
+        }
+    }
+}
